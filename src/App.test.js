9b7d384b9e2df9de1edcd2b4688c4fd2433e18d0
Andreas Pieger <andreas.pieger@epg.com>
2020-01-07 16:15:44 +0100

    Updated Test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { StitchAuthProvider } from "components/Auth/StitchAuth";
+import * as serviceWorker from './serviceWorker';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import './index.css';
+
+library.add(fab)
+
+test('renders app', () => {
+  const { getByText } = render(
+    <StitchAuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </StitchAuthProvider>);
 });

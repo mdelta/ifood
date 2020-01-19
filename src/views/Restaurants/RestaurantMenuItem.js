cc3fb0345cfde0e36510c8374d4c32ab9a209917
Andreas Pieger <andreas.pieger@epg.com>
2020-01-19 22:35:18 +0100

    prototype for new list style

--- src/views/Restaurants/RestaurantMenuItem.js
+++ src/views/Restaurants/RestaurantMenuItem.js
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { Component } from 'react'
 
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
 
-import FolderIcon from '@material-ui/icons/Folder';
-import ViewIcon from '@material-ui/icons/Visibility';
+import FolderIcon from '@material-ui/icons/Folder'
+import ViewIcon from '@material-ui/icons/Visibility'
+import CallIcon from '@material-ui/icons/Call'
 
-export default function RestaurantItem(props) {
+class RestaurantItem extends Component {
+  render () {
+    let menuIcon
+    let telphone
+
+    if (this.props.menu) {
+      menuIcon =
+        <IconButton href={this.props.menu} edge='end' aria-label='view'>
+          <ViewIcon />
+        </IconButton>
+    }
+
+    if (this.props.phone) {
+      const tel_phone = 'tel:' + this.props.phone
+      telphone =
+        <IconButton href={tel_phone} edge='end' aria-label='call'>
+          <CallIcon />
+        </IconButton>
+    }
+
     return (
-        <ListItem>
-            <ListItemAvatar>
-                <Avatar>
-                    <FolderIcon />
-                </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-                primary={props.name}
-                secondary={props.phone}
-            />
-            <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="view">
-                    <ViewIcon />
-                </IconButton>
-            </ListItemSecondaryAction>
-        </ListItem>
-    );
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <FolderIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary={this.props.name}
+          secondary={this.props.phone}
+        />
+        <ListItemSecondaryAction>
+          {menuIcon}
+          {telphone}
+
+        </ListItemSecondaryAction>
+      </ListItem>
+    )
+  }
 }
+
+export default RestaurantItem
--- src/views/Restaurants/RestaurantMenuList.js
+++ src/views/Restaurants/RestaurantMenuList.js
@@ -1,68 +1,93 @@
-import React from 'react';
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
-import List from '@material-ui/core/List';
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+import List from '@material-ui/core/List'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Card from 'components/Card/Card.js'
+import CardBody from 'components/Card/CardBody.js'
+import CardHeader from 'components/Card/CardHeader.js'
+import CardFooter from 'components/Card/CardFooter.js'
+import PropTypes from 'prop-types'
 
-import RestaurantMenuItem from "./RestaurantMenuItem";
-
-import styles from "assets/jss/material-kit-react/views/loginPage.js";
-import image from "assets/img/bg7.jpg";
-
-const useStyles = makeStyles(styles);
-
-const useListStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        maxWidth: 752,
-    },
-    demo: {
-        color: 'black',
-        backgroundColor: theme.palette.background.paper,
-    },
-    title: {
-        margin: theme.spacing(4, 0, 2),
-    },
-}));
+import RestaurantMenuItem from './RestaurantMenuItem'
 
 RestaurantList.propTypes = {
-    restaurants: PropTypes.array,
-    actions: PropTypes.object,
-};
+  restaurants: PropTypes.array,
+  actions: PropTypes.object
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    // height: '100vh',
+    overflow: 'auto'
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column'
+  },
+  card: {
+    paddingLeft: 10,
+    paddingRight: 10
+  }
+}))
+
 export default function RestaurantList(props) {
-    const classes = useStyles();
-    const listClasses = useListStyles();
-    const { restaurants } = props;
+  const classes = useStyles();
+  const { restaurants } = props;
 
-    return (
-        <div
-            className={classes.pageHeader}
-            style={{
-                backgroundImage: "url(" + image + ")",
-                backgroundSize: "cover",
-                backgroundPosition: "top center"
-            }}
-        >
-            <div className={classes.container}>
-                <GridContainer justify="center">
-                    <GridItem xs={12} sm={12} md={12} >
-                        <div className={listClasses.demo}>
-                            <List>
-                                {restaurants.length === 0 && <p></p>}
-                                {restaurants.map(restaurant => (
-                                    <RestaurantMenuItem
-                                        key={restaurant._id.toString()}
-                                        name={restaurant.name}
-                                        phone={restaurant.contact.telephone}
-                                    />
-                                ))}
-                            </List>
-                        </div>
-                    </GridItem>
-                </GridContainer>
-            </div>
-        </div>
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
 
-    );
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth='lg' className={classes.container}>
+          <Card className={classes.card}>
+            <CardHeader color='primary' className={classes.cardHeader}>
+              <h2>Menu</h2>
+            </CardHeader>
+            <CardBody>
+              <p>
+                See the restaurant menu or call the restaurant directly.
+              </p>
+            </CardBody>
+            <CardFooter className={classes.cardFooter} />
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={12} lg={12}>
+                  <List>
+                    {restaurants.length === 0 && <p />}
+                    {restaurants.map(restaurant => (
+                      <RestaurantMenuItem
+                        key={restaurant._id.toString()}
+                        name={restaurant.name}
+                        phone={restaurant.contact.telephone}
+                        menu={restaurant.menu}
+                      />
+                    ))}
+                  </List>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                  {/* <Orders /> */}
+                </Paper>
+              </Grid>
+            </Grid>
+          </Card>
+        </Container>
+      </main>
+    </div>
+  )
 }

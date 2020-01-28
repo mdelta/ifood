5b66a16f41575bac4fb41a833c12a39ca5bb08b7
Andreas Pieger <andreas.pieger@epg.com>
2020-01-28 08:23:12 +0100

    Environment from environment variable (try)

--- src/index.js
+++ src/index.js
@@ -10,11 +10,14 @@ import App from 'App'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+const { environment } = require('./config')
+console.log(`Your env is ${environment}`)
 library.add(fab)
 
 Sentry.init({
   dsn: 'https://266d4489fe284b61a90dea2ea9689b28@sentry.io/1877567',
-  release: 'dev' // TODO see https://docs.sentry.io/workflow/releases/?_ga=2.225325146.1970574778.1579103240-1392631938.1578668509&platform=javascript#configure-sdk
+  release: 'dev', // TODO see https://docs.sentry.io/workflow/releases/?_ga=2.225325146.1970574778.1579103240-1392631938.1578668509&platform=javascript#configure-sdk
+  environment: environment
 })
 
 ReactDOM.render(
